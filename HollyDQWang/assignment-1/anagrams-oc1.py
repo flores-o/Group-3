f479c705016959c5416bdf7acee9f3c07ee79e36
Dongqing Wang <hollywangdq@gmail.com>
2019-05-18 23:12:58 -0400

    added assignment-1 and optional challenge 1

---
+++ HollyDQWang/assignment-1/anagrams-oc1.py
@@ -0,0 +1,28 @@
+def areAnagram(str1, str2):
+    '''
+    Given two strings, determine if one is an anagram of the other.
+    Two words are anagrams of each other if they are made of the same letters
+    in a different order.
+
+    Optional Challenge 1: Case insensitive.
+
+    For example:
+    - "listen" and "silent" are anagrams
+    - "triangle" and "integral" are anagrams
+    -  "apple" and "pabble" are NOT anagrams
+    '''
+    str1_len = len(str1)
+    str2_len = len(str2)
+
+    if str1_len != str2_len:
+        return False
+
+    str1_sorted = sorted(str1.lower())
+    str2_sorted = sorted(str2.lower())
+
+    for x in range(str1_len):
+        if str1_sorted[x] != str2_sorted[x]:
+            print(str2_sorted[x].lower())
+            return False
+
+    return True
