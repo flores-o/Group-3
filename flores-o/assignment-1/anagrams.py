1a3c3435e1360a9aa5bc8a96466b7e7542d152a3
Oana Florescu <35745326+flores-o@users.noreply.github.com>
2019-05-24 15:23:58 +0300

    Add files via upload

--- flores-o/assignment-1/anagrams.py
+++ flores-o/assignment-1/anagrams.py
@@ -4,10 +4,22 @@ class Solution:
     def __init__(self):
         pass
     def check_anagrams(self, s1, s2):
+        """returns True if the 2 given strings are anagrams
 
+        Args: 
+            s1: string
+            s2: string
+        Returns:
+           Bool
+        """
         if len(s1) != len(s2):
             return False
-
+        # char_frequency is a dictionary that maps a character to its 
+        # number of occurences
+        # for every character in s1, we increase its frequency with 1
+        # for every character in s2, we decrease its frequency with 1
+        # we return true if the char_frequency only contains pair of 
+        # form (character, 0)
         char_frequency = defaultdict(int)
         for c in s1:
             char_frequency[c] += 1
@@ -19,6 +31,7 @@ class Solution:
             if freq != 0:
                 return False
         return True
+
 """
 if __name__ == '__main__':
     s = Solution()
