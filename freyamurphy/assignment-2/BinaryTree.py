538d8823fa2ee7549b52043143b3c3a91d848352
freyamurphy <freyamurphy@gmail.com>
2019-05-26 15:57:13 +0100

    Added docstrings
    Added descriptions for each method and class in line with the style guide recommended by Jenny in this week's meeting.

--- freyamurphy/assignment-2/BinaryTree.py
+++ freyamurphy/assignment-2/BinaryTree.py
@@ -1,12 +1,32 @@
 import Node
 
 class BinaryTree:
+    """A binary tree made up of Nodes.
+
+    Each Node has up to two children.
+
+    Attributes:
+        root: The root Node of the tree, which has no parent.
+    """
 
     def __init__(self, rootNode):
+        """Inits BinaryTree with a root Node."""
         self.root = rootNode
 
     def insert(self, childNode, parentNode, insertLeft):
-        # TODO: Check if childNode.key is already in tree
+        """Adds a Node to the binary tree.
+
+        Args:
+            childNode: The Node to be added to the tree.
+            parentNode: The Node which childNode will be a child of.
+            insertLeft: A boolean indicating whether to make childNode the left
+                or right child of parentNode.
+
+        Raises:
+            Exception: Tried to add a child to a reference that was not None.
+        """
+        # TODO: Check if childNode.key is already in tree.
+        # TODO: Check if parentNode is actually part of the tree.
 
         if insertLeft and parentNode.left == None:
             parentNode.left = childNode
@@ -15,11 +35,21 @@ class BinaryTree:
         else:
             raise Exception("Cannot insert where a child already exists.")
 
-    # Return reference to Node with key
-    # If key does not exist, return None
-    # Based on pre-order traversal algorithm from:
-    # https://www.tutorialspoint.com/python/python_tree_traversal_algorithms.htm
     def getNodeWithKey(self, key, startingNode):
+        """Gets a reference to the Node with a specific key.
+
+        Based on pre-order traversal algorithm from:
+        https://www.tutorialspoint.com/python/python_tree_traversal_algorithms.htm
+
+        Args:
+            key: The unique Node identifier to search for. Can be any type.
+            startingNode: This Node and its left and right subtree will be
+                searched; the rest of the binary tree is ignored.
+
+        Returns:
+            A reference to the Node with attribute key equal to parameter key.
+            None if there is no suitable Node is found.
+        """
 
         if startingNode is not None:
             if startingNode.key == key:
@@ -32,6 +62,21 @@ class BinaryTree:
                     return self.getNodeWithKey(key, startingNode.right)
 
     def getParent(self, key, startingNode, parent):
+        """Gets a reference to the parent of a Node with a specific key.
+
+        Args:
+            key: A unique identifier of the Node whose parent we are searching
+                for. Can be any type.
+            startingNode: This node and its left and right subtree will be
+                searched; the rest of the binary tree is ignored.
+            parent: The parent Node of startingNode.
+
+        Returns:
+            A reference to a Node which is the parent of the Node with attribute
+            key equal to parameter key.
+            None if there is no Node with the searched for key or if this Node
+            has no parent.
+        """
 
         if startingNode is not None:
             if startingNode.key == key:
@@ -49,6 +94,16 @@ class BinaryTree:
 
 
     def getAncestors(self, key):
+        """ Gets a list containing all ancestors of the Node with a specified
+            key.
+
+        Args:
+            key: The key of the Node to get the ancestors of. Can be any type.
+
+        Returns:
+            A list of keys of the ancestors of the Node with attribute key equal
+            to parameter key.
+        """
 
         parent = self.getParent(key, self.root, None)
 
--- freyamurphy/assignment-2/Node.py
+++ freyamurphy/assignment-2/Node.py
@@ -2,8 +2,19 @@
 # https://www.tutorialspoint.com/python/python_binary_tree.htm
 
 class Node:
+    """A node in a BinaryTree.
+
+    Attributes:
+        left: The Node which is the left child of this Node. Can be None.
+        right: The Node which is the right child of this Node. Can be None.
+        key: A unique identifier of any type for the Node.
+    """
 
     def __init__(self, key):
+        """Inits Node with key.
+
+        Initially a Node has no children.
+        """
         self.left = None
         self.right = None
         self.key = key
